c0cce727de2b7a0901a198f533c271a178345c29
wqx081 <wangqx@mpreader.com>
2016-11-14 20:00:27 +0800

    First commit

---
+++ src/ant/util/async_util.h
@@ -0,0 +1,55 @@
+// Utility functions which are handy when doing async/callback-based programming.
+#ifndef KUDU_UTIL_ASYNC_UTIL_H
+#define KUDU_UTIL_ASYNC_UTIL_H
+
+#include "ant/base/bind.h"
+#include "ant/base/macros.h"
+#include "ant/util/countdown_latch.h"
+#include "ant/util/status.h"
+#include "ant/util/status_callback.h"
+
+namespace ant {
+
+// Simple class which can be used to make async methods synchronous.
+// For example:
+//   Synchronizer s;
+//   SomeAsyncMethod(s.callback());
+//   CHECK_OK(s.Wait());
+class Synchronizer {
+ public:
+  Synchronizer()
+    : l(1) {
+  }
+  void StatusCB(const Status& status) {
+    s = status;
+    l.CountDown();
+  }
+  StatusCallback AsStatusCallback() {
+    // Synchronizers are often declared on the stack, so it doesn't make
+    // sense for a callback to take a reference to its synchronizer.
+    //
+    // Note: this means the returned callback _must_ go out of scope before
+    // its synchronizer.
+    return Bind(&Synchronizer::StatusCB, Unretained(this));
+  }
+  Status Wait() {
+    l.Wait();
+    return s;
+  }
+  Status WaitFor(const MonoDelta& delta) {
+    if (PREDICT_FALSE(!l.WaitFor(delta))) {
+      return Status::TimedOut("Timed out while waiting for the callback to be called.");
+    }
+    return s;
+  }
+  void Reset() {
+    l.Reset(1);
+  }
+ private:
+  DISALLOW_COPY_AND_ASSIGN(Synchronizer);
+  Status s;
+  CountDownLatch l;
+};
+
+} // namespace ant
+#endif /* KUDU_UTIL_ASYNC_UTIL_H */
